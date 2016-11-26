6117199af6fce04c0d8029052c044e54a2f0b58b
Jirka <del@hs-furtwangen.de>
2016-11-26 14:03:50 +0100

    Mein eigenen Server!

--- test.js
+++ test.js
@@ -1,1 +1,25 @@
-console.log("Hello World");
+//Lets require/import the HTTP module
+var http = require('http');
+//Lets define a port we want to listen to
+var port = process.env.PORT;
+if (port == undefined)
+	port = 8080;
+
+//Create a server
+var server = http.createServer(handleRequest);
+//Lets start our server
+server.listen(port, onListening);
+
+//We need a function which handles requests and send response
+function handleRequest(request, response) {
+	response.writeHead(200, {
+		"content-type" : "text/html"
+	});
+	response.write("Hello World<br>");
+	response.write("Process: " + process + "<br>");
+	response.end('Klappt! Request url: ' + request.url);
+}
+
+function onListening() {
+	console.log("Yup...Server listening on: http://localhost:%s", port);
+}
