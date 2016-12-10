ffeca323670f66398a2c5693bb7d1a69fd60b9a2
Jirka <del@hs-furtwangen.de>
2016-12-10 19:41:06 +0100

    CORS implemented
    Simply add "Access-Control-Allow-Origin":"*" to header

--- test.js
+++ test.js
@@ -13,26 +13,10 @@ var server = http.createServer(handleRequest);
 server.listen(port, onListening);
 // Handles request and send response
 function handleRequest(request, response) {
-    console.log("Incoming call");
-    console.log("METHOD: " + request.method);
-    /*
-        if (request.method === "OPTIONS") {
-            console.log("PREFLIGHT");
-            response.writeHead(200, {
-                "Access-Control-Allow-Origin": "*",
-                "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,OPTIONS",
-                "Access-Control-Allow-Headers": "Content-Type, Authorization, Content-Length, X-Requested-With"
-            });
-            response.end();
-            return;
-        }
-    */
     response.writeHead(200, {
         "Access-Control-Allow-Origin": "*", "content-type": "text/html"
     });
     response.write("Heroku/Github/Mongo-Test<br>");
-    response.write("appearantly needs procfile, though package.json may be omitted when required packages are installed<br>");
-    response.write("created this text on new branch TestBranch<br>");
     response.write("NODE_ENV: " + process.env.NODE_ENV + "<br>");
     response.write("Calls to this page: " + ++counter + "<br>");
     response.write("Port: " + port + "<br>");
