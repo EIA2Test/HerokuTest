52d06ed7799e70a9898c9ce8ff445bcd693544e0
Jirka <del@hs-furtwangen.de>
2018-01-03 11:24:34 +0100

    back to existing database

--- mongo.ts
+++ mongo.ts
@@ -15,7 +15,7 @@ console.log(fs.existsSync("mongo.js"));
 
 var databaseURL: string = "mongodb://localhost:27017/test";
 if (process.env.NODE_ENV == "production")
-    databaseURL = "mongodb://EIA2Test:EIA2Test1!@ds119368.mlab.com:19368/eia2test2";
+    databaseURL = "mongodb://EIA2Test:EIA2Test1!@ds119368.mlab.com:19368/eia2test";
 mongodb.MongoClient.connect(databaseURL, onConnect);
 
 var db: mongodb.Db;
