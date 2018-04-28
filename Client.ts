90296363ac9ed2d62b01b7df959705fc416cda54
Jirka <del@hs-furtwangen.de>
2018-04-28 16:15:50 +0200

    Tests

--- Client.ts
+++ Client.ts
@@ -1,6 +1,6 @@
 namespace ServerSentEventsTest {
     window.addEventListener("load", init);
-
+  
     function init(_event: Event): void {
         var eventSource: EventSource = new EventSource("http://localhost:8100");
         eventSource.addEventListener("test", handleServerSentEvent);
